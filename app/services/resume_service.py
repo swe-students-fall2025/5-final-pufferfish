941810f7e1e997266c60867b99fe09d991088987
phoebelh <153004907+phoebelh@users.noreply.github.com>
2025-12-07 02:46:05 -0500

    update: resume service to get reviewer info

--- app/services/resume_service.py
+++ app/services/resume_service.py
@@ -2,15 +2,47 @@ from app.extensions import mongo
 
 class ResumeService:
     @staticmethod
-    def get_highlights(document_id):
-        doc = mongo.db.highlights.find_one({"document_id": document_id})
+    @staticmethod
+    def get_highlights(document_id, reviewer_id=None):
+        query = {"document_id": document_id}
+        if reviewer_id:
+            query["reviewer_id"] = reviewer_id
+            
+        doc = mongo.db.highlights.find_one(query)
         return doc.get("highlights", {}) if doc else {}
 
     @staticmethod
-    def save_highlights(document_id, highlights):
+    def get_all_reviews(document_id):
+        # returns a list of all review documents for this resume
+        cursor = mongo.db.highlights.find({"document_id": document_id})
+        reviews = []
+        for doc in cursor:
+            reviews.append({
+                "reviewer_id": doc.get("reviewer_id"),
+                "reviewer_name": doc.get("reviewer_name", "Anonymous"),
+                "highlights": doc.get("highlights", {})
+            })
+        return reviews
+
+    @staticmethod
+    def save_highlights(document_id, highlights, reviewer_id=None, reviewer_name="Anonymous"):
+        # composite key on document_id AND reviewer_id
+        query = {"document_id": document_id}
+        if reviewer_id:
+            query["reviewer_id"] = reviewer_id
+            
+        update_data = {
+            "highlights": highlights,
+            "document_id": document_id
+        }
+        if reviewer_id:
+            update_data["reviewer_id"] = reviewer_id
+        if reviewer_name:
+            update_data["reviewer_name"] = reviewer_name
+
         mongo.db.highlights.update_one(
-            {"document_id": document_id},
-            {"$set": {"highlights": highlights}},
+            query,
+            {"$set": update_data},
             upsert=True
         )
 
